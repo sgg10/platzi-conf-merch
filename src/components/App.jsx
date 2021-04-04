9ce1fcda3c00f7e7686b9747c101b322f86873ab
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-04-04 18:40:56 -0500

    Layout

--- src/components/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "../styles/App.scss";
-
-const App = () => <h1>Hola mundo</h1>;
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/components/Header.scss";
+
+const Header = () => {
+	return (
+		<div className='Header'>
+			<h1 className='Header-title'>Platzi Conf Merch</h1>
+			<div className='Header-checkout'>Checkout</div>
+		</div>
+	);
+};
+
+export default Header;
